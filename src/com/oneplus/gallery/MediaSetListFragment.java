744594468bb5e62911d9d97212146a35c1000ee9
Gavin Kuo <Gavin.Kuo@oneplus.tw>
2015-09-23 16:16:34 +0800

    Add simple UI in MediaSetListFragment.

--- src/com/oneplus/gallery/MediaSetListFragment.java
+++ src/com/oneplus/gallery/MediaSetListFragment.java
@@ -1,9 +1,13 @@
 package com.oneplus.gallery;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.oneplus.base.BaseFragment;
@@ -13,6 +17,11 @@ import com.oneplus.base.BaseFragment;
  */
 public class MediaSetListFragment extends BaseFragment
 {
+	// Fields
+	private Activity m_Activity;
+	private MediaSetListAdapter m_MediaSetListAdapter;
+	private ListView m_MediaSetListView;
+	
 	/**
 	 * Initialize new MediaSetListFragment instance.
 	 */
@@ -22,13 +31,78 @@ public class MediaSetListFragment extends BaseFragment
 	}
 	
 	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		m_Activity =  this.getActivity();
+		m_MediaSetListAdapter = new MediaSetListAdapter();
+	}
+
 	// Create view.
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		//
-		TextView tv = new TextView(this.getActivity());
-		tv.setText(this.TAG);
-		return tv;
+		return inflater.inflate(R.layout.fragment_media_set_list, container, false);
 	}
+
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		m_MediaSetListView = (ListView)getView().findViewById(R.id.media_set_listview);
+		m_MediaSetListView.setAdapter(m_MediaSetListAdapter);
+	}
+	
+	private class MediaSetListAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return 5;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			ViewInfo viewInfo;
+			if(convertView == null)
+			{
+				LayoutInflater inflater = m_Activity.getLayoutInflater();
+				convertView = inflater.inflate(R.layout.layout_media_set_list_item, parent, false);
+				
+				viewInfo = new ViewInfo();
+				viewInfo.titleText = (TextView)convertView.findViewById(R.id.media_set_title);
+				viewInfo.sizeTextView = (TextView)convertView.findViewById(R.id.media_set_size);
+				viewInfo.coverImage = (ImageView)convertView.findViewById(R.id.media_set_cover_image);
+				
+				convertView.setTag(viewInfo);
+			}
+			else
+				viewInfo = (ViewInfo)convertView.getTag();
+			
+			return convertView;
+		}
+		
+	}
+	
+	// Class to keep menu item information in related view.
+	private static final class ViewInfo
+	{
+		public TextView titleText;
+		public TextView sizeTextView; 
+		public ImageView coverImage;
+	}
+	
 }
